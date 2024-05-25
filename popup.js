d58ad0c0f29eb7c703004b3f99155980416403a7
shubhamashish33 <shubhamashish8@gmail.com>
2024-05-26 01:04:29 +0530

    bug fixes

--- popup.js
+++ popup.js
@@ -5,9 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const italicButton = document.getElementById("italicButton");
   const underlineButton = document.getElementById("underlineButton");
   const notesList = document.getElementById("notesList");
-
   let countof = 0;
-
   if (chrome.storage) {
     chrome.storage.local.get("notes", function (result) {
       if (result.notes) {
@@ -17,7 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
         chrome.action.setBadgeText({ text: String(countof) });
       }
     });
-
     saveButton.addEventListener("click", addNote);
     boldButton.addEventListener("click", function () {
       formatText("bold");
@@ -28,13 +25,11 @@ document.addEventListener("DOMContentLoaded", function () {
     underlineButton.addEventListener("click", function () {
       formatText("underline");
     });
-
     noteInput.addEventListener("keydown", function (event) {
       if (event.keyCode === 13) {
         addNote();
       }
     });
-
     function addNote() {
       const noteText = noteInput.value.trim();
       if (noteText !== "") {
@@ -42,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
           const existingNotes = result.notes ? JSON.parse(result.notes) : [];
           const newNote = { text: noteText, id: Date.now() };
           existingNotes.push(newNote);
-
           chrome.storage.local.set(
             { notes: JSON.stringify(existingNotes) },
             function () {
@@ -64,12 +58,10 @@ document.addEventListener("DOMContentLoaded", function () {
         italicButton.classList.toggle("active")
       }
     }
-
     function displayNote(note) {
       const noteDiv = document.createElement("div");
       noteDiv.classList.add("note");
       noteDiv.innerHTML = applyFormatting(note.text);
-
       const removeIcon = document.createElement("i");
       removeIcon.classList.add("fas", "fa-times", "remove-icon");
       removeIcon.addEventListener("click", function () {
@@ -78,11 +70,9 @@ document.addEventListener("DOMContentLoaded", function () {
         countof--;
         chrome.action.setBadgeText({ text: String(countof) });
       });
-
       noteDiv.appendChild(removeIcon);
       notesList.appendChild(noteDiv);
     }
-
     function removeNoteFromStorage(noteId) {
       chrome.storage.local.get("notes", function (result) {
         const existingNotes = result.notes ? JSON.parse(result.notes) : [];
@@ -90,25 +80,19 @@ document.addEventListener("DOMContentLoaded", function () {
         chrome.storage.local.set({ notes: JSON.stringify(updatedNotes) });
       });
     }
-
     function applyFormatting(text) {
       let formattedText = text;
-
       if (boldButton.classList.contains("active")) {
         formattedText = `<b>${formattedText}</b>`;
       }
-
       if (italicButton.classList.contains("active")) {
         formattedText = `<i>${formattedText}</i>`;
       }
-
       if (underlineButton.classList.contains("active")) {
         formattedText = `<u>${formattedText}</u>`;
       }
-
       return formattedText;
     }
-
     function formatText(format) {
       switch (format) {
         case "bold":
@@ -132,7 +116,6 @@ document.addEventListener("DOMContentLoaded", function () {
         default:
           break;
       }
-
       noteInput.value = applyFormatting(noteInput.value);
     }
   } else {
